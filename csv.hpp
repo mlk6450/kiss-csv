9c8eedf6e9e588c602a139c3c2a0baa5abd52a2c
Matthew Krebs <mlk6450@rit.edu>
2023-03-27 13:10:01 -0400

    added default delimiter ','

--- csv.hpp
+++ csv.hpp
@@ -5,14 +5,16 @@
 #include <vector>
 #include <iostream>
 
+#define DEFUALT_DELIMITER (',')
+
 namespace CSV
 {
   typedef std::string Cell;
   typedef std::vector<Cell> Row;
   typedef std::vector<Row> Table;
 
-  Row parseLine(const std::string& line, char delimiter);
-  Table parseFile(const std::string& filename, char delimiter);
+  Row parseLine(const std::string& line, char delimiter = DEFUALT_DELIMITER);
+  Table parseFile(const std::string& filename, char delimiter = DEFUALT_DELIMITER);
 
   std::ostream& operator<<(std::ostream& os, Row row);
   std::ostream& operator<<(std::ostream& os, Table table);
